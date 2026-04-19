715352bcbf04a82925daa5ab8e5d755221715184
jgyy <jeffrey.goh@sit.singaporetech.edu.sg>
2026-04-19 16:40:39 +0800

    fix: use Vector2 in IWorldReader, add MonoGame ref to test project

--- src/StardewBot/GameState/IWorldReader.cs
+++ src/StardewBot/GameState/IWorldReader.cs
@@ -1,3 +1,4 @@
+using Microsoft.Xna.Framework;
 using System.Collections.Generic;
 
 namespace StardewBot.GameState;
@@ -7,9 +8,9 @@ public interface IWorldReader
     float EnergyPercent { get; }
     float InventoryFillRatio { get; }
     int MineFloorReached { get; }
-    IReadOnlyList<(float X, float Y)> CropsToWater { get; }
-    IReadOnlyList<(float X, float Y)> CropsToHarvest { get; }
-    IReadOnlyList<(float X, float Y)> ForagablePositions { get; }
+    IReadOnlyList<Vector2> CropsToWater { get; }
+    IReadOnlyList<Vector2> CropsToHarvest { get; }
+    IReadOnlyList<Vector2> ForagablePositions { get; }
     IReadOnlyList<NpcInfo> Npcs { get; }
     bool IsRaining { get; }
 }
--- tests/StardewBot.Tests/Fakes/FakeWorldReader.cs
+++ tests/StardewBot.Tests/Fakes/FakeWorldReader.cs
@@ -1,3 +1,4 @@
+using Microsoft.Xna.Framework;
 using StardewBot.GameState;
 using System.Collections.Generic;
 
@@ -8,9 +9,9 @@ public class FakeWorldReader : IWorldReader
     public float EnergyPercent { get; set; } = 1.0f;
     public float InventoryFillRatio { get; set; } = 0.0f;
     public int MineFloorReached { get; set; } = 0;
-    public IReadOnlyList<(float X, float Y)> CropsToWater { get; set; } = new List<(float X, float Y)>();
-    public IReadOnlyList<(float X, float Y)> CropsToHarvest { get; set; } = new List<(float X, float Y)>();
-    public IReadOnlyList<(float X, float Y)> ForagablePositions { get; set; } = new List<(float X, float Y)>();
+    public IReadOnlyList<Vector2> CropsToWater { get; set; } = new List<Vector2>();
+    public IReadOnlyList<Vector2> CropsToHarvest { get; set; } = new List<Vector2>();
+    public IReadOnlyList<Vector2> ForagablePositions { get; set; } = new List<Vector2>();
     public IReadOnlyList<NpcInfo> Npcs { get; set; } = new List<NpcInfo>();
     public bool IsRaining { get; set; } = false;
 }
