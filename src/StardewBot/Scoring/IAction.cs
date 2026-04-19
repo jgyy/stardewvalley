b70a405bdd4d701655ae2a39166806927941b538
jgyy <jeffrey.goh@sit.singaporetech.edu.sg>
2026-04-19 19:05:26 +0800

    chore: remove comment

--- src/StardewBot/Scoring/IAction.cs
+++ src/StardewBot/Scoring/IAction.cs
@@ -6,12 +6,9 @@ public interface IAction
 {
     string Name { get; }
 
-    // Returns score 0-100+ (higher = higher priority today)
     float Score(DayContext ctx, IWorldReader world);
 
-    // Called once when action is selected; capture state here
     void Begin(DayContext ctx, IWorldReader world);
 
-    // Called each UpdateTicked; return true when the action is complete
     bool Tick();
 }
